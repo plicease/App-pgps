f65f894f684f3a0b9b1dd1d1e222ebf2cb2401ab
Graham Ollis <plicease@cpan.org>
2016-06-15 11:38:35 -0400

    fixes

--- lib/App/pgps.pm
+++ lib/App/pgps.pm
@@ -1,15 +1,14 @@
+package App::pgps;
+
 use strict;
 use warnings;
-use 5.020;
-use experimental 'postderef';
-use experimental 'signatures';
+use 5.012;
+use DBI;
+use Test2::Util::Table qw( table );
+use Getopt::Long qw( GetOptionsFromArray );
 
-package App::pgps {
-
-  # ABSTRACT: PostgreSQL ps
-
-  use DBI;
-  use Test2::Util::Table qw( table );
+# ABSTRACT: PostgreSQL ps
+# VERSION
 
 =head1 METHODS
 
@@ -19,39 +18,44 @@ package App::pgps {
 
 =cut
 
-  sub main ($class, @args)
+sub main
+{
+  my($class, @args) = @_;
+  
+  my $active = 0;
+  
+  GetOptionsFromArray(\@args, "a" => \$active);
+  
+  my $dbh = DBI->connect('dbi:Pg:dbname=postgres', '', '');
+    
+  my $sth = $dbh->prepare(join ' ', do { no warnings; qw{
+    SELECT
+      pid,
+      usename,
+      client_addr,
+      state,
+      CASE WHEN state = 'active' THEN query ELSE '' END
+    FROM
+      pg_stat_activity
+  }});
+    
+  $sth->execute;
+    
+  my @rows;
+  while(my $row = $sth->fetchrow_arrayref)
   {
-    my $dbh = DBI->connect('dbi:Pg:dbname=postgres', '', '');
-    
-    my $sth = $dbh->prepare(join ' ', do { no warnings; qw{
-      SELECT
-        pid,
-        usename,
-        client_addr,
-        state,
-        CASE WHEN state = 'active' THEN query ELSE '' END
-      FROM
-        pg_stat_activity
-    }});
-    
-    $sth->execute;
-    
-    my @rows;
-    while(my $row = $sth->fetchrow_arrayref)
-    {
-      push @rows, [@$row];
-    }
-
-    say $_ for table(
-      collapse => 1,
-      header   => [qw( pid user address state query )],
-      sanitize => 1,
-      rows     => \@rows,
-    );    
-    
+    next if $active && $row->[3] eq 'idle';
+    push @rows, [@$row];
   }
 
-  0;
+  say $_ for table(
+    collapse => 1,
+    header   => [qw( pid user address state query )],
+    sanitize => 1,
+    rows     => \@rows,
+  );    
+
+  0;    
 }
 
 1;
