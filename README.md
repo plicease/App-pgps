58b73bd3d17b1adcd845e63e273a13261e0be206
Graham Ollis <plicease@cpan.org>
2016-06-15 11:27:34 -0400

    initial implementation

---
+++ README.md
@@ -0,0 +1,24 @@
+# App::pgps [![Build Status](https://secure.travis-ci.org/plicease/App-pgps.png)](http://travis-ci.org/plicease/App-pgps)
+
+PostgreSQL ps
+
+# METHODS
+
+## main
+
+    App::pgps->main(@arguments);
+
+# SEE ALSO
+
+[pgps](https://metacpan.org/pod/pgps)
+
+# AUTHOR
+
+Graham Ollis <plicease@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2016 by Graham Ollis.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/App/pgps.pm
+++ lib/App/pgps.pm
@@ -1,9 +1,65 @@
-package App::pgps;
-
 use strict;
 use warnings;
+use 5.020;
+use experimental 'postderef';
+use experimental 'signatures';
 
-# ABSTRACT: PostgreSQL ps
-# VERSION
+package App::pgps {
+
+  # ABSTRACT: PostgreSQL ps
+
+  use DBI;
+  use Test2::Util::Table qw( table );
+
+=head1 METHODS
+
+=head2 main
+
+ App::pgps->main(@arguments);
+
+=cut
+
+  sub main ($class, @args)
+  {
+    my $dbh = DBI->connect('dbi:Pg:dbname=postgres', '', '');
+    
+    my $sth = $dbh->prepare(join ' ', do { no warnings; qw{
+      SELECT
+        pid,
+        usename,
+        client_addr,
+        state,
+        CASE WHEN state = 'active' THEN query ELSE '' END
+      FROM
+        pg_stat_activity
+    }});
+    
+    $sth->execute;
+    
+    my @rows;
+    while(my $row = $sth->fetchrow_arrayref)
+    {
+      push @rows, [@$row];
+    }
+
+    say $_ for table(
+      collapse => 1,
+      header   => [qw( pid user address state query )],
+      sanitize => 1,
+      rows     => \@rows,
+    );    
+    
+  }
+
+  0;
+}
 
 1;
+
+=head1 SEE ALSO
+
+L<pgps>
+
+=cut
+
+
