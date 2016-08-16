b0e9cd1a3afc99222aa4325412932b458b950363
Graham Ollis <plicease@cpan.org>
2016-08-16 15:10:12 -0400

    fatpack

--- fat/build.sh
+++ fat/build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-rm -rf pgps
+rm -rf pgps pgkill
 cd ..
 export PERL5LIB=`pwd`/lib:$PERL5LIB
+
 fatpack pack bin/pgps > fat/pgps
 chmod +x fat/pgps
+fatpack pack bin/pgkill > fat/pgkill
+chmod +x fat/pgkill
+
 rm -rf fatlib
